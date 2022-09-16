f41bf5078120ea713abfcc33372eaaf7df244711
MKoushikYadav <105151574+MKoushikYadav@users.noreply.github.com>
2022-09-17 02:48:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,68 @@ element.reportValidity() ;
 element.setCustomValidity('');
 } }
 
-
-
 const dob = document.getElementById ('dob') ;
 dob.addEventListener( 'input' , () => validatedob(dob) ) ;
 function validatedob(element) {
-let age= Math.abs(new Date() - Date(dob));
 if(element.validity.rangeUnderflow || element.validity.rangeOverflow) {
 element.setCustomValidity("Age<18 or age>51") ;
 element.reportValidity() ;
-return false;
 } else{
 element.setCustomValidity('');
-return true;
 } }
+
+
+
+
+let userForm = document.getElementById("user-form") ;
+
+const retrieveEntries = () => {
+let entries = localStorage.getItem("user-entries");
+if (entries) {
+entries = JSON.parse(entries)
+}else {
+entries = [];
+}
+return entries;
+}
+
+let userEntries = retrieveEntries();
+
+const saveUserForm = (event) => {
+event.preventDefault();
+const name = document .getElementById ("name" ).value;
+const email = document.getElementById ("email").value;
+const password = document.getElementById("password").value;
+const dob = document.getElementById( "dob" ).value;
+const acceptedTermsAndconditions = document.getElementById("acceptTerms").checked;
+
+const entry = {
+name,
+email,
+password,
+dob,
+acceptedTermsAndconditions
+};
+userEntries.push(entry) ;
+localStorage.setItem("user-entries", JSON.stringify(userEntries));
+displayEntries();
+}
+
+
+
+const displayEntries = () =>{
+    const entries = retrieveEntries();
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td >${entry.name}</td>`;
+        const emailCell =`<td>${entry.email}</td>`;
+        const passwordCell  =`<td>${entry.password}</td>`;
+        const dobCell = `<td>${entry.dob}</td>`;
+        const acceptTermsCell = `<td>${entry.acceptedTermsAndconditions}</td>`
+        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+    return row;}).join(`\n`);
+    const table = `<table ><tr><th>Name</th><th>Email</th><th>Password</th><th>dob</th><th>accepted terms</th></tr>${tableEntries} </table>`;
+    let details = document.getElementById('user-entries');
+    details.innerHTML = table;
+    }
+userForm.addEventListener("submit", saveUserForm) ;
+displayEntries();
